2cb5be8a6c1420914b462d2911a0205396bfa555
Хасанов Артур Ильгизович <ai_khasanov@cs.local>
2022-10-16 23:23:34 +0300

    added firebase init

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_chat_app/core/constants/constants.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: Constants.apiKey,
+        appId: Constants.appId,
+        messagingSenderId: Constants.messagingSenderId,
+        projectId: Constants.projectId,
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(const ChatApp());
+}
+
+class ChatApp extends StatelessWidget {
+  const ChatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
